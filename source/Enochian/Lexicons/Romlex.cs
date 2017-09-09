d3e2c2d2c0ce6a846cf1ff0062c0685c979e0f70
Gordon Tisher <gordon@balafon.net>
2017-09-09 13:32:41 -0700

    romlex datatypes

---
+++ source/Enochian/Lexicons/Romlex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enochian.Lexicons
+{
+    public class Romlex
+    {
+    }
+
+    public class RomlexLexicon
+    {
+        public IList<RomlexLanguage> Languages { get; set; }
+        public IList<RomlexEntry> Entries { get; set; }
+    }
+
+    public class RomlexLanguage
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class RomlexEntry
+    {
+        public string SrcLangCode { get; set; }
+        public string DefLangCode { get; set; }
+
+        public string Lemma { get; set; }
+        public string PartOfSpeech { get; set; }
+        public string Definition { get; set; }
+    }
+}
